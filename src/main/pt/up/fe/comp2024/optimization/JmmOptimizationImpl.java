7e42765162788da0dd2d637bd5e038db845706f2
gpinto03 <gongaspinto03@gmail.com>
2024-04-08 15:05:40 +0100

    started working on ollir

--- src/main/pt/up/fe/comp2024/optimization/JmmOptimizationImpl.java
+++ src/main/pt/up/fe/comp2024/optimization/JmmOptimizationImpl.java
@@ -3,6 +3,9 @@ package pt.up.fe.comp2024.optimization;
 import pt.up.fe.comp.jmm.analysis.JmmSemanticsResult;
 import pt.up.fe.comp.jmm.ollir.JmmOptimization;
 import pt.up.fe.comp.jmm.ollir.OllirResult;
+import pt.up.fe.comp.jmm.ast.JmmNode;
+import pt.up.fe.comp2024.symboltable.JmmSymbolTable;
+
 
 import java.util.Collections;
 
@@ -10,11 +13,27 @@ public class JmmOptimizationImpl implements JmmOptimization {
 
     @Override
     public OllirResult toOllir(JmmSemanticsResult semanticsResult) {
+        JmmNode rootNode = semanticsResult.getRootNode();
+        OllirGeneratorVisitor visitor = new OllirGeneratorVisitor(semanticsResult.getSymbolTable());
 
-        var visitor = new OllirGeneratorVisitor(semanticsResult.getSymbolTable());
-        var ollirCode = visitor.visit(semanticsResult.getRootNode());
+        System.out.println("\n\n\nGenerating OLLIR ...");
+        String ollirCode = (String) visitor.visit(rootNode);
 
+        System.out.println("\nOllir code: ");
+        System.out.println(ollirCode);
+
+        return new OllirResult(semanticsResult, ollirCode, semanticsResult.getReports());
+
+        /*
+
+        var ollirVisitor = new OllirGeneratorVisitor(semanticsResult.getSymbolTable());
+        ollirVisitor.visit(semanticsResult.getRootNode());
+
+        String ollirCode = ollirVisitor.toString();
         return new OllirResult(semanticsResult, ollirCode, Collections.emptyList());
+
+         */
+
     }
 
     @Override
