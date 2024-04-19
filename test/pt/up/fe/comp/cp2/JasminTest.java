375a396eb4b5b3521756bfb2172d222297186904
Rui Carvalho <ruipedrobc@gmail.com>
2024-04-19 18:58:47 +0100

    added auxiliar test to test some details

--- test/pt/up/fe/comp/cp2/JasminTest.java
+++ test/pt/up/fe/comp/cp2/JasminTest.java
@@ -13,6 +13,16 @@ import static org.junit.Assert.assertEquals;
 
 public class JasminTest {
 
+    // new test
+    @Test
+    public void testHelloWorld() {
+        var code = SpecsIo.getResource("pt/up/fe/comp/cp2/apps/HelloWorld.jmm");
+        var jasminResult = TestUtils.backend(code, Collections.emptyMap());
+        System.out.println(jasminResult.getJasminCode());
+        var result = TestUtils.runJasmin(jasminResult.getJasminCode(), Collections.emptyMap());
+        assertEquals("Hello, World!", result.strip());
+    }
+
     @Test
     public void ollirToJasminBasic() {
         testOllirToJasmin("pt/up/fe/comp/cp2/jasmin/OllirToJasminBasic.ollir");
