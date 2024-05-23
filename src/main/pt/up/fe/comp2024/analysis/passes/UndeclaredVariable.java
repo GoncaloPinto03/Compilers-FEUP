f9181c33001592d44b175790e38601235d71ec3a
Goncalo Santos <up202108839@up.pt>
2024-05-23 16:46:03 +0100

    revert

--- src/main/pt/up/fe/comp2024/analysis/passes/UndeclaredVariable.java
+++ src/main/pt/up/fe/comp2024/analysis/passes/UndeclaredVariable.java
@@ -673,16 +673,7 @@ public class UndeclaredVariable extends AnalysisVisitor {
             return null;
         }
 
-        if(!lhsNode.getKind().equals("VarRefExpr")){
-            String message = "Should be a varrefexpr";
-            addReport(Report.newError(
-                    Stage.SEMANTIC,
-                    NodeUtils.getLine(assignStmt),
-                    NodeUtils.getColumn(assignStmt),
-                    message,
-                    null
-            ));
-        }
+
 
         if(lhsType.getName().equals(rhsType.getName()) && lhsType.isArray() == rhsType.isArray()){
             return null;
